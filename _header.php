ba03dc98ab60e6e17606c158f0abd2703bf29986
unknown <ala.johan@gmail.com>
2022-03-18 14:00:58 +0100

    modif du header avec un debut de nav

--- _header.php
+++ _header.php
@@ -1,1 +1,25 @@
-     <header> NAV</header>
+<body>
+
+     <?php ?>
+
+
+
+     <header>
+          <nav class="navbar">
+               <div class="logo">
+                    <a href="index.html" class="history"><img src="https://fakeimg.pl/100x100/?text=logo" alt="" /></a>
+               </div>
+               <div>
+                    <a href="index.html" class="url">Home </a>
+
+                    <a href="heroes.html" class="url">Trombinoscope</a>
+
+                    <a href="inventaire.html" class="url">People</a>
+                    <a href="contact.html" class="url">Contact</a>
+               </div>
+          </nav>
+     </header>
+
+</body>
+
+</html>

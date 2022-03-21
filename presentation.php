0551b2fea28fb2f3562e863213ea7248e43b7e2c
Marysa <regentmarysapro@gmail.com>
2022-03-21 23:34:23 +0100

    changed color, first version presentation.php

--- presentation.php
+++ presentation.php
@@ -14,14 +14,43 @@
 
     <main>
         
-        <div class ="logo-flex">
-        <img src="https://fakeimg.pl/200/">
-        </div>
+        <section class ="logo-flex">
+        <img src="https://picsum.photos/200/200">
+        </section>
 
-        <div class ="team">
-        <h2 class ="h2prez">Présentation de la team DePhants</h2>  
-        </div>
+        <section class ="team">
+            <h2>Présentation de la team DePhants</h2>
+            <p class ="p-team">Huic Arabia est conserta, ex alio latere Nabataeis contigua; <br> 
+            opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.<br> 
+            Haec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas.<br>
+            Hanc provinciae inposito nomine rectoreque adtributo obtemperare legibus nostris Traianus conpulit imperator incolarum tumore saepe contunso cum glorioso marte Mediam urgeret et Parthos.<br>
+            Huic Arabia est conserta, ex alio latere Nabataeis contigua; <br> 
+            opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.<br> 
+            Haec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas.<br>
+            Hanc provinciae inposito nomine rectoreque adtributo obtemperare legibus nostris Traianus conpulit imperator incolarum tumore saepe contunso cum glorioso marte Mediam urgeret et Parthos.</p><br>
+        </section>
 
+        <section class ="projets">
+            <h2>
+            Projets réalisés
+            </h2>
+
+            <div class ="article">
+            <img src="https://picsum.photos/100/100">
+            <h3>CV</h3>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</article>
+            </div>
+            <img src="https://picsum.photos/100/100">
+            <h3>Site de formation</h3>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</article>
+            </div>
+
+            <img src="https://picsum.photos/200/200">
+            <h3>Trombinoscope</h3>
+            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</article>
+            </div>
+            </div>
+        </section>
     </main>
 
     <?php include("_footer.php"); ?>

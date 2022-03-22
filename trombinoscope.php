b22d679e635fb41119e40bd932271621300d3e5c
Mélissa <melissa-callejon_student2022@wilder.school>
2022-03-22 09:55:54 +0100

    structure html + css

--- trombinoscope.php
+++ trombinoscope.php
@@ -12,36 +12,146 @@
 <body>
     <?php include("_header.php"); ?>
     <main>
-        <h1>Trombi</h1>
+
+        <h1>Trombinoscope de la TEAM dePhants</h1>
         <article class="trombi-grid">
-            <div class="grid-span-2">
+        
+            <div class="grid-span-2 photo-gris">
                 <div class="trombi-flexbox-left">
-                    <img src="https://fakeimg.pl/200/">
-                    <p>text</p>
+                    <img src="https://media-exp1.licdn.com/dms/image/C5603AQHC3fR0PZemuQ/profile-displayphoto-shrink_400_400/0/1551954922697?e=1653523200&v=beta&t=7KJFUMm9EiQ7uXyr9WYSej_yEohYP6SrdEiaCTAsLVw">
+                    <div class="fullname-button">
+                        <h3>Jean-François Morin</h3>
+                        <p>The Big Boss</p>
+                        <button href="#" class="button-6" role="button">En savoir plus</button>
+                    </div>
                 </div>
-
             </div>
-            <div class="grid-span-2">
 
+            <div class="grid-span-2 photo-gris">
                 <div class="trombi-flex-right">
-                    <p>text</p>
-                    <img src="https://fakeimg.pl/200/">
+                    <div class="fullname-button">
+                        <h3 class="justify-text-right">Johan Ala</h3>
+                        <p>Développeur Web Full-Stack Junior</p>
+                        <button class="button-6" role="button">En savoir plus</button>
+                    </div>
+                    <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFOlxNGPV7N3A/profile-displayphoto-shrink_400_400/0/1646042804627?e=1653523200&v=beta&t=AF_iIWcW-S-HqwJLC9LqM8-yISYfxLQT8hoxmG7uyXo">
                 </div>
             </div>
-            <div class="grid-span-2">
-                <div class="trombi-flexbox-left"><img src="https://fakeimg.pl/200/">
-                    <p>text</p>
+
+            <div class="grid-span-2 photo-gris">
+                <div class="trombi-flexbox-left">
+                    <img src="https://media-exp1.licdn.com/dms/image/D4E03AQGSx6JjNXFccA/profile-displayphoto-shrink_400_400/0/1645378996632?e=1653523200&v=beta&t=0ubkVPwo9Xz_u_7QDlD9yOfRxFLFPewgWtCE0vP0QPg">
+                    <div class="fullname-button">
+                    <h3>Hervé Rasamimanantsoa</h3>
+                    <p>Développeur Web Full-Stack Junior</p>
+                    <button class="button-6" role="button">En savoir plus</button>
                 </div>
-
+                </div>
             </div>
-            <div class="grid-span-2">
 
+            <div class="grid-span-2 photo-gris">
                 <div class="trombi-flex-right">
-                    <p>text</p><img src="https://fakeimg.pl/200/">
-
+                    <div class="fullname-button">
+                        <h3 class="justify-text-right">Marysa Régent</h3>
+                        <p>Développeuse Web Full-Stack Junior</p>
+                        <button class="button-6" role="button">En savoir plus</button>
+                    </div>
+                    <img src="https://media.discordapp.net/attachments/947831240667574272/955455615545770074/Capture_decran_2022-03-21_a_14.18.48.png">
                 </div>
             </div>
+
+            <div class="grid-span-2 photo-gris">
+                <div class="trombi-flexbox-left">
+                    <img src="https://scontent.fcdg4-1.fna.fbcdn.net/v/t1.6435-9/95321121_1141650146168555_1101025577978036224_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=174925&_nc_ohc=nulJJzyEUVAAX86avKv&tn=xPTEkl2JOyIbtI4a&_nc_ht=scontent.fcdg4-1.fna&oh=00_AT8a8xucGRt3OYXVbqlokVrY7Z8XocNzVHFC7NkK6851Fw&oe=625ABCE7">
+                    <div class="fullname-button">
+                    <h3>Mélissa Callejon</h3>
+                    <p>Développeuse Web Full-Stack Junior</p>
+                    <button class="button-6" role="button">En savoir plus</button>
+                </div>
+                </div>
+            </div>
+
+            <div class="grid-span-2 photo-gris">
+                <div class="trombi-flex-right">
+                    <div class="fullname-button">
+                        <h3 class="justify-text-right">Tushar Chowdhury</h3>
+                        <p>Développeur Web Full-Stack Junior</p>
+                        <button class="button-6" role="button">En savoir plus</button>
+                    </div>
+                    <img src="https://media.discordapp.net/attachments/952953034525274112/955497430277840956/jean-francois-morin-formateur-php.png">
+                </div>
+            </div>
+
+            <div class="grid-span-2 photo-gris">
+                <div class="trombi-flexbox-left">
+                    <img src="https://media.discordapp.net/attachments/947831240667574272/955455729416953936/Capture_decran_2022-03-21_a_14.19.51.png">
+                    <div class="fullname-button">
+                    <h3>Alexandre Guyonnet</h3>
+                    <p>Développeur Web Full-Stack Junior</p>
+                    <button class="button-6" role="button">En savoir plus</button>
+                </div>
+                </div>
+            </div>
+
+            <div class="grid-span-2 photo-gris">
+                <div class="trombi-flex-right">
+                    <div class="fullname-button">
+                        <h3 class="justify-text-right">Kévin Lonsot</h3>
+                        <p>Développeur Web Full-Stack Junior</p>
+                        <button class="button-6" role="button">En savoir plus</button>
+                    </div>
+                    <img src="https://media.discordapp.net/attachments/955475208209326201/955475717217472512/Kevin_Lansot_45_-_copie.jpg?width=1359&height=2038">
+                </div>
+            </div>
+
+            <div class="grid-span-2 photo-gris">
+                <div class="trombi-flexbox-left">
+                    <img src="https://media.discordapp.net/attachments/952582409973534791/955117858722029588/Capture_decran_2022-03-20_a_15.56.17.png"> 
+                    <div class="fullname-button">
+                    <h3>Cyril Fougeroux</h3>
+                    <p>Développeur Web Full-Stack Junior</p>
+                    <button class="button-6" role="button">En savoir plus</button>
+                </div>
+                </div>
+
+            </div>
+
+            <div class="grid-span-2 photo-gris">
+                <div class="trombi-flex-right">
+                    <div class="fullname-button">
+                    <h3 class="justify-text-right">Jessica Pêcheux</h3>
+                    <p>Développeuse Web Full-Stack Junior</p>
+                        <button class="button-6" role="button">En savoir plus</button>
+                    </div>
+                    <img src="https://media-exp1.licdn.com/dms/image/D4E35AQEeMfDRZdGWjQ/profile-framedphoto-shrink_400_400/0/1647022525466?e=1647864000&v=beta&t=2uL-z72FXlcrAW3tI5QoT7y2Yu5XpJ98dD-SELRlC7A">
+                </div>
+            </div>
+
+            <div class="grid-span-2 photo-gris">
+                <div class="trombi-flexbox-left">
+                    <img src="https://media.discordapp.net/attachments/947831240667574272/955455616183328828/Capture_decran_2022-03-21_a_14.19.07.png">
+                    <div class="fullname-button">
+                    <h3>Yoann Brambilla</h3>
+                    <p>Développeur Web Full-Stack Junior</p>
+                    <button class="button-6" role="button">En savoir plus</button>
+                </div>
+                </div>
+
+            </div>
+
+            <div class="grid-span-2 photo-gris">
+                <div class="trombi-flex-right">
+                    <div class="fullname-button">
+                        <h3 class="justify-text-right">Mickaël Desbiens</h3>
+                        <p>Développeur Web Full-Stack Junior</p>
+                        <button class="button-6" role="button">En savoir plus</button>
+                    </div>
+                    <img src="https://media.discordapp.net/attachments/947831240667574272/955456090689110066/Capture_decran_2022-03-21_a_14.21.15.png">
+                </div>
+            </div>
+
         </article>
+
     </main>
 
     <?php include("_footer.php"); ?>

01b93ea838a6a0f78ebcaac140ad8f9b72691793
H Rasamy <HRasam@HRasamy.lan>
2022-03-23 11:32:09 +0100

    changed form

--- contact.php
+++ contact.php
@@ -13,30 +13,61 @@
     <?php include("_header.php"); ?>
     <main>
 
+    <h2>Contactez-nous/devenez membre</h2>
+    <hr class="separation"></hr>
+    
+
     <form action="contact.php" method="post">
         <div class="container">
-            <h2>Contact us/become a member</h2>
-            <hr class="separation"></hr>
+            
+            
             <div class="form">
+            <label for="nom"></label>
+            <input type="text" id="name" placeholder= "Nom">
 
-                <input type="text" name = "name" placeholder= "Nom">
-                <input type="text" name = "prénom" placeholder= "Prénom">
-                <input type="email" name = "email" placeholder= "@mail">
-                <input type="text" name = " téléphone" placeholder= "Téléphone">
-                <input type="text" name = "objet" placeholder= "Objet">
-                <textarea placeholder= "Your message"></textarea>
-                <button type="submit">Envoyer</button>
+            <label for="prénom"></label>
+            <input type="text" id="prénom" placeholder= "Prénom">
+
+            <label for="email"></label>
+            <input type="email" id="email" placeholder= "@mail">
+            <label for="linkedin"></label>
+            <input type="url" id="linkedin" placeholder= "Linkedin">
+
+            
+
+            
+
+            
+            <label for="profil-select"></label>
+            <select name="profil" id="profil-select">
+                <option value="ton-profil" disabled selected>Ton profil</option>
+                <option value="back-end">Back-end</option>
+                <option value="front-end">Front-end</option>
+                <option value="full-stack">Full-stack</option>
+            </select>
+
+
+            <label for="message"></label>
+            <textarea id="message" placeholder= "Your message"></textarea>
+            <label for="picture">Photo de profil</label>
+            <input type="file" id="picture" name="picture"
+                    accept="image/png, image/jpeg">
+            <button type="submit">Envoyer</button>
+
+            
+
+            
+            
 
             </div>
         </div>
 
     </form>
-</p>
+
 
     </main>
 
     <?php include("_footer.php"); ?>
     <script src="script.js"></script>
 </body>
-
 </html>

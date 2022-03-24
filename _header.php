e2fe6dc78a10e2136d638f1010ee8072be12ac49
unknown <ala.johan@gmail.com>
2022-03-24 19:51:45 +0100

    rename index

--- _header.php
+++ _header.php
@@ -13,7 +13,7 @@
           <nav>
                <div class="burger"><span></span></div>
                <ul>
-                    <li><a class="one" href="/index.php">Accueil </a></li>
+                    <li><a class="one" href="/trombinoscope.php">Accueil </a></li>
                     <li><a class="one" href="/presentation.php">Team dePhants </a></li>
                     <li "><a class= " one" href="/contact.php">Contact</a></li>
                </ul>

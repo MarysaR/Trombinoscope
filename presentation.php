49fb5398e3f898c71cf84ee719cbcd754de45e36
Marysa <regentmarysapro@gmail.com>
2022-03-24 16:13:28 +0100

    changed presentation

--- presentation.php
+++ presentation.php
@@ -18,38 +18,56 @@
         <img src="https://picsum.photos/200/200">
         </section>
 
-        
+        <h2 class ="h2-left">Présentation de la team DePhants</h2>
+
         <section class ="team">
-            <div><p class ="p-team">Huic Arabia est conserta, ex alio latere Nabataeis contigua; <br> 
-            opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.<br> 
-            Haec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas.<br>
-            Hanc provinciae inposito nomine rectoreque adtributo obtemperare legibus nostris Traianus conpulit imperator incolarum tumore saepe contunso cum glorioso marte Mediam urgeret et Parthos.<br>
-            Huic Arabia est conserta, ex alio latere Nabataeis contigua; <br> 
-            opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.<br> 
-            Haec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas.<br>
-            Hanc provinciae inposito nomine rectoreque adtributo obtemperare legibus nostris Traianus conpulit imperator incolarum tumore saepe contunso cum glorioso marte Mediam urgeret et Parthos.</p><br></div>
-            <div><h2>Présentation de la team DePhants</h2></div>
+
+            <div class ="dephants">
+             <img src="https://picsum.photos/250/150">
+            </div>
+
+            <div class ="p-team">
+                <p>Huic Arabia est conserta, ex alio latere Nabataeis contigua; <br> 
+                opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.<br> 
+                Haec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas.<br>
+                Hanc provinciae inposito nomine rectoreque adtributo obtemperare legibus nostris Traianus conpulit imperator incolarum tumore saepe contunso cum glorioso marte Mediam urgeret et Parthos.<br>
+                </p><br>
+            </div>
+            
         </section>
 
         <section class ="projets">
-            <h2>
-            Projets réalisés
-            </h2>
 
             <div class ="article">
-            <img src="https://picsum.photos/100/100">
-            <h3>CV</h3>
-            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</article>
-            </div>
-            <img src="https://picsum.photos/100/100">
-            <h3>Site de formation</h3>
-            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</article>
-            </div>
+                <h2 class = "h2-projet">Projets réalisés</h2>
+                <div class ="cv">
+                    <div>
+                        <h3>CV</h3>
+                    </div>
 
-            <img src="https://picsum.photos/200/200">
-            <h3>Trombinoscope</h3>
-            <div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</article>
-            </div>
+                    <div class = p-cv>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</div>
+                    </div>
+
+                <div class ="formation">
+                    <div>
+                        <h3>Site de formation</h3> 
+                    </div>
+                    <div class = p-formation>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</p> 
+                    </div>
+                </div>
+
+                <div class ="trombinoscope">
+                    <div>
+                        <h3>Trombinoscope</h3>
+                    </div>
+
+                    <div class ="p-trombinoscope">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</p>
+                    </div>
+                </div>
+
             </div>
         </section>
     </main>

e770af8878f3fbdab264c8032c783e0d2774af83
Marysa <regentmarysapro@gmail.com>
2022-03-25 12:55:16 +0100

    final changed

--- presentation.php
+++ presentation.php
@@ -13,62 +13,73 @@
     <?php include("_header.php"); ?>
 
     <main>
-        
-        <section class ="logo-flex">
-        <img src="https://picsum.photos/200/200">
-        </section>
 
-        <h2 class ="h2-left">Présentation de la team DePhants</h2>
+        <h2 class="h2-left">Présentation de la team DePhants</h2>
 
-        <section class ="team">
-
-            <div class ="dephants">
-             <img src="https://picsum.photos/250/150">
-            </div>
-
-            <div class ="p-team">
-                <p>Huic Arabia est conserta, ex alio latere Nabataeis contigua; <br> 
-                opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.<br> 
-                Haec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas.<br>
-                Hanc provinciae inposito nomine rectoreque adtributo obtemperare legibus nostris Traianus conpulit imperator incolarum tumore saepe contunso cum glorioso marte Mediam urgeret et Parthos.<br>
-                </p><br>
-            </div>
-            
-        </section>
-
-        <section class ="projets">
-
-            <div class ="article">
-                <h2 class = "h2-projet">Projets réalisés</h2>
-                <div class ="cv">
-                    <div>
-                        <h3>CV</h3>
-                    </div>
-
-                    <div class = p-cv>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</div>
-                    </div>
-
-                <div class ="formation">
-                    <div>
-                        <h3>Site de formation</h3> 
-                    </div>
-                    <div class = p-formation>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</p> 
-                    </div>
+        <section class="container-presentation team">
+            <div class="team-text">
+                <div class="dephants">
+                    <img class="img-team" src="https://picsum.photos/250/150">
                 </div>
 
-                <div class ="trombinoscope">
-                    <div>
-                        <h3>Trombinoscope</h3>
+                <div class="p-team">
+                    <p>Huic Arabia est conserta, ex alio latere Nabataeis contigua; <br>
+                        opima varietate conmerciorum castrisque oppleta validis et castellis, quae ad repellendos gentium vicinarum excursus sollicitudo pervigil veterum per oportunos saltus erexit et cautos.<br>
+                        Haec quoque civitates habet inter oppida quaedam ingentes Bostram et Gerasam atque Philadelphiam murorum firmitate cautissimas.<br>
+                    </p><br>
+                </div>
+            </div>
+        </section>
+
+        <h2 class="h2-projet">Projets réalisés</h2>
+
+        <section class="container-presentation projets">
+            <div class="cv-text">
+                <div class="cv">
+                    <div class="cv-photo">
+                        <img src="https://picsum.photos/250/150">
                     </div>
 
-                    <div class ="p-trombinoscope">
+                    <div class=p-cv>
+                        <h3>Site multipages - CV</h3>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <div class="container-presentation form-text">
+            <div class="formation">
+                <div class="cv">
+
+
+                    <div class="p-formation">
+                        <h3>Plateforme de formation</h3>
                         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</p>
                     </div>
+                    <div class="form-img">
+                        <img src="https://picsum.photos/250/150">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="trombinoscope">
+
+            <h3>Site multipages - Trombinoscope</h3>
+
+            <div class="container-trombinoscope">
+                <div class="trombi-photo">
+                    <img src="https://picsum.photos/250/150">
                 </div>
 
+                <div class="p-trombinoscope">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam inventore alias eos! Error earum dolorum a, quas similique illum corrupti numquam adipisci tempore animi ex amet cumque natus soluta ipsam.</p>
+                </div>
             </div>
+
+        </div>
+        </div>
         </section>
     </main>
 
